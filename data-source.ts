f99449ee526a5a911e98e1204b2669ce771a837b
sneogi-cci <46446268+sneogi-cci@users.noreply.github.com>
2024-11-21 12:30:16 +0530

    Change connection to postgresql

--- data-source.ts
+++ data-source.ts
@@ -1,7 +1,11 @@
 import { DataSource, DataSourceOptions } from "typeorm";
 export const dataSourceOptions: DataSourceOptions = {
-  type: 'sqlite',
-  database: 'db.sqlite',
+  type: 'postgres',
+  host: 'localhost',
+  database: 'mycv',
+  port: 5432,
+  password: 'admin',
+  username: 'postgres',
   entities: ['**/*.entity.js'],
   synchronize:true,
   migrations: ["dist/migrations/*.js"],
